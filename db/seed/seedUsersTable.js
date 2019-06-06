49b8b57151543e96eb8a2a208f2aa0bc40d07334
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-06 15:51:25 -0400

    Improve seeding script

--- db/seed/seedUsersTable.js
+++ db/seed/seedUsersTable.js
@@ -9,9 +9,13 @@ const pgPort = process.env.PGPORT || 5432;
 
 const copyFrom = require('pg-copy-streams').from;
 const Readable = require('stream').Readable;
-const seedCount = process.env.SEEDCOUNT || 20000000;
+const seedCount = process.env.SEEDCOUNT || 1000000;
+const iterations = process.env.ITERATIONS || 1;
+let currentIteration = 0;
 
-console.log(`🚀 Attempt to seed ${seedCount} records to database ${pgDatabase} at ${pgHost}:${pgPort}`);
+console.log(
+	`🚀 Attempt to seed ${seedCount} records x ${iterations} times to database ${pgDatabase} at ${pgHost}:${pgPort}`
+);
 
 const pool = new Pool({
 	user: pgUser,
@@ -22,14 +26,29 @@ const pool = new Pool({
 });
 
 console.time('seedTime');
-pool.connect().then((client) => {
-	let done = () => {
-		client.release();
-	};
 
-	const stream = client.query(copyFrom('COPY users (first_name,last_name,email,avatar_url,created_at) FROM STDIN'));
-	const rs = new Readable({
-		read() {
+const seedDb = () => {
+	currentIteration++;
+
+	console.log(`✈️ Iteration #${currentIteration}`);
+
+	pool.connect().then((client) => {
+		let done = () => {
+			console.log('Stream ended');
+			client.release();
+			if (currentIteration < iterations) {
+				seedDb();
+			}
+		};
+
+		let count = 0;
+
+		const stream = client.query(
+			copyFrom('COPY users (first_name,last_name,email,avatar_url,created_at) FROM STDIN')
+		);
+		const rs = new Readable();
+
+		rs._read = () => {
 			if (count >= seedCount) {
 				rs.push(null);
 			} else {
@@ -47,21 +66,21 @@ pool.connect().then((client) => {
 				);
 				count++;
 			}
-		}
+		};
+
+		let onError = (strErr) => {
+			console.error('Something went wrong:', strErr);
+			done();
+		};
+
+		rs.on('error', onError);
+		stream.on('error', onError);
+		stream.on('end', done);
+		rs.pipe(stream);
 	});
+};
 
-	let count = 0;
-
-	let onError = (strErr) => {
-		console.error('Something went wrong:', strErr);
-		done();
-	};
-
-	rs.on('error', onError);
-	stream.on('error', onError);
-	stream.on('end', done);
-	rs.pipe(stream);
-});
+seedDb();
 
 process.on('exit', () => {
 	console.timeEnd('seedTime');
