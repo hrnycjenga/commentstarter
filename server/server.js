9848084a51a7fd689062ad010f458cfaf3553c95
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-11 11:44:39 -0400

    Implement cors

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const parser = require('body-parser');
 const router = require(path.resolve(__dirname, 'router.js'));
+const cors = require('cors');
 
 app.use(parser.json());
+app.use(cors());
 app.use('/:projId', express.static(path.join(__dirname, '/../client/dist')));
 app.use('/static', express.static(path.join(__dirname, '/../client/dist/static')));
 
