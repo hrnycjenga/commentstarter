985d8b4bd26ee1fa7c954e43fa6ff747bc57a5fe
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-14 16:32:50 -0400

    Remove db queries

--- server/modelSql.js
+++ server/modelSql.js
@@ -16,17 +16,17 @@ const pool = new Pool({
 const queryMessages = async (projectId) => {
 	const query = `SELECT c.*, u.first_name, u.last_name, u.avatar_url, u.email FROM comments c INNER JOIN users u ON c.author_id = u.id
 								WHERE c.project_id = ${projectId}`;
-	try {
-		// console.log(`🚀 Attempt to connect to database ${pgDatabase} at ${pgHost}:${pgPort}`);
-		// client = await pool.connect();
+	// try {
+	// 	// console.log(`🚀 Attempt to connect to database ${pgDatabase} at ${pgHost}:${pgPort}`);
+	// 	// client = await pool.connect();
 
-		// result = await client.query(query);
-		let result = await pool.query(query);
-		return result.rows;
-	} catch (err) {
-		return Promise.reject(err);
-	}
-	// return [ { true: true } ];
+	// 	// result = await client.query(query);
+	// 	result = await pool.query(query);
+	// 	return result.rows;
+	// } catch (err) {
+	// 	return Promise.reject(err);
+	// }
+	return [ { true: true } ];
 };
 
 const queryReplies = async (messageId) => {
@@ -88,6 +88,5 @@ module.exports = {
 	queryMessages,
 	queryReplies,
 	queryUserMessages,
-	insertMessage,
-	pool
+	insertMessage
 };
