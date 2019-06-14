651674c8aa941fdc3deed9b77087267d0e89ddc4
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-14 16:34:50 -0400

    Fix query

--- server/controller.js
+++ server/controller.js
@@ -1,27 +1,16 @@
 const path = require('path');
 const model = require(path.resolve(__dirname, 'modelSql.js'));
 
-const getMessages = (req, res) => {
+const getMessages = async (req, res) => {
 	let projectId = req.params.projId;
 
-	// try {
-	// 	const data = await model.queryMessages(projectId);
-	// 	res.status(200).json(data);
-	// } catch (err) {
-	// 	res.status(400).send();
-	// 	console.log(err);
-	// }
-
-	const query = `SELECT c.*, u.first_name, u.last_name, u.avatar_url, u.email FROM comments c INNER JOIN users u ON c.author_id = u.id
-								WHERE c.project_id = ${projectId}`;
-	model.pool.query(query, (err, result) => {
-		if (err) {
-			res.status(400).send();
-			return console.log(err);
-		} else {
-			return res.status(200).json(result.rows);
-		}
-	});
+	try {
+		const data = await model.queryMessages(projectId);
+		res.status(200).json(data);
+	} catch (err) {
+		res.status(400).send();
+		console.log(err);
+	}
 };
 
 const getRepliesToComment = (req, res) => {
